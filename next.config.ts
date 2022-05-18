3c11f4b23a9fdea7d548a523eb87d12f12a3083f
Franco Borgiani <franco@nookalabs.com>
2022-05-18 20:13:44 -0300

    [FIX] next.config.js

--- next.config.ts
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-export const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ["raw.githubusercontent.com"]
-  }
-}
-
-module.exports = nextConfig
